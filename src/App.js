6bcf3d4a0eb01291d8ed4b4836fbda835d22bc62
huunhat22 <huunhatth07@gmail.com>
2021-05-06 23:58:23 +0700

    Nhận các props và setSate

--- src/App.js
+++ src/App.js
@@ -12,15 +12,34 @@ class App extends Component {
     super(props);
     this.state = {
       color : 'red',
-      fontsize : 15
+      fontsize : 14
     };
   }
 
+  //function set color 
   onSetColor = (param) =>{
     // console.log(param);
     this.setState({
       color : param
     });
+  }
+
+  //function set size when we action click button Giam -Tang
+  setSize = (paramSize) =>{
+    // console.log(paramSize);
+    this.setState({
+      fontsize : (this.state.fontsize + paramSize >= 8 && this.state.fontsize + paramSize <= 36) ? this.state.fontsize + paramSize :this.state.fontsize
+    });
+  }
+
+  //function resetValue khi co su kien click vao button Reset
+  resetValue = (paramflag) =>{
+    if (paramflag) {
+      this.setState({
+        color : 'red',
+        fontsize : 14
+      });
+    }
   }
 
   render(){
@@ -29,10 +48,10 @@ class App extends Component {
           <div className='row mt-5'>
             <Colorpicker colorRecive = {this.state.color} onReciveColor = {this.onSetColor}></Colorpicker>
             <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-              <Sizesetting></Sizesetting>
-              <Reset></Reset>
+              <Sizesetting fontSize = {this.state.fontsize} reciveChangeSize = {this.setSize}></Sizesetting>
+              <Reset reciveReset = {this.resetValue}></Reset>
             </div>
-            <Result colorRecive = {this.state.color}></Result>
+            <Result colorRecive = {this.state.color} fontSize = {this.state.fontsize}></Result>
           </div>
         </div>
     );
