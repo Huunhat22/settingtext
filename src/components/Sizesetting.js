baddef5af01ed20946b57a0fba6157c8280221cc
huunhat22 <huunhatth07@gmail.com>
2021-05-06 23:57:49 +0700

    xử lý sự kiện khi nhấn nút tăng giảm

--- src/components/Sizesetting.js
+++ src/components/Sizesetting.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 
 class Sizesetting extends Component{
+
+    //function onChangeSize
+    onChangeSize =(value) =>{
+        this.props.reciveChangeSize(value);
+    }
+
     render(){
         return(
             <div className="card mb-3">
                 <div className="card-header">
-                    <h3>Size : 15px</h3>
+                    <h3>Size : {this.props.fontSize}</h3>
                 </div>
                 <div className="card-body">
-                    <button type="button" className="btn btn-success" >Giảm</button>&nbsp;
-                    <button type="button" className="btn btn-success" >Tăng</button>
+                    <button type="button" className="btn btn-success" onClick = {() =>this.onChangeSize(-2) }>Giảm</button>&nbsp;
+                    <button type="button" className="btn btn-success" onClick = {() =>this.onChangeSize(+2) }>Tăng</button>
                 </div>
             </div>
         );
