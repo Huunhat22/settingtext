b1bbfaada627c37cf72cc289e26f67979073b44a
huunhat22 <huunhatth07@gmail.com>
2021-05-06 23:57:02 +0700

    sử lý sự kiện khi nhấn button Reset

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,9 +1,14 @@
 import { Component } from 'react';
 
 class Reset extends Component{
+    //tạo function onResetDefault
+    onResetDefault = () =>{
+        this.props.reciveReset(true);
+    }
+
     render(){
         return(
-            <button type="button" className="btn btn-primary" >reset</button>
+            <button type="button" className="btn btn-primary" onClick = {() => this.onResetDefault() }>reset</button>
         );
     }
 }
